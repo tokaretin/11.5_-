a31f17de2409e5b15ae990cad65e581138e53b75
tokaretin <tokaretin@gmail.com>
2023-04-11 11:46:44 +0200

    Добавьте файлы проекта.

---
+++ 11.5_Типы и приведения между ними/11.5_Типы и приведения между ними.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    {
+        int a = 10;
+        float b = (float)a;
+
+        cout << b << '\n'; // Вывод на экран: 10
+    }
+
+    {
+        int c = 'a';
+        int x = (int)c;
+
+        cout << x << '\n'; // Вывод на экран: 97 (Таблица ASCII)
+    }
+
+    {
+        int x = 98;
+        char c = (char)x;
+
+        cout << c << '\n'; // Вывод на экран: b (Таблица ASCII)
+    }
+
+    {
+        bool b = true; // true always 1 and false always 0
+        int x = (int)b;
+
+        cout << x << '\n'; // Вывод на экран: 1
+    }
+
+    {
+        int x = 0;
+        bool b = (bool)x;
+
+        cout << b << '\n'; // Вывод на экран: 0
+    }
+
+    {
+        int x = -100; // если число с минусом, оно тоже является true = 1
+        bool b = (bool)x;
+         
+        cout << b << '\n'; // Вывод на экран: 1
+    }
+    
+}
